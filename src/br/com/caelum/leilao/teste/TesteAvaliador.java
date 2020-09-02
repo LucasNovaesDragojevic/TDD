6756f0d6cd54ed212287dd70ba1b3b0782628c61
Lucas Novaes Dragojevic <lucas.dragojevic@gmail.com>
2020-09-02 13:16:11 -0300

    Criados testes de ordem crescente, decresente e aleatória do leilão

--- src/br/com/caelum/leilao/teste/TesteAvaliador.java
+++ src/br/com/caelum/leilao/teste/TesteAvaliador.java
@@ -1,6 +1,9 @@
 package br.com.caelum.leilao.teste;
 
-import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
 import org.junit.Test;
 
 import br.com.caelum.leilao.dominio.Lance;
@@ -28,7 +31,91 @@ public class TesteAvaliador {
 		double menorEsperado = 250;
 		double maiorEsperado = 400;
 		
-		Assert.assertEquals(maiorEsperado, leiloeiro.getMaiorLance(), 0.00001);
-		Assert.assertEquals(menorEsperado, leiloeiro.getMenorLance(), 0.00001);
+		assertEquals(maiorEsperado, leiloeiro.getMaiorLance(), 0.00001);
+		assertEquals(menorEsperado, leiloeiro.getMenorLance(), 0.00001);
+	}
+	
+	@Test
+	public void deveEntenderLancesEmOrdemDecrescente() {
+		Usuario joao = new Usuario("João");
+		Usuario maria = new Usuario("Maria");
+		
+		Leilao leilao = new Leilao("RTX 2080 TI");
+		
+		leilao.propoe(new Lance(joao, 400.00));
+		leilao.propoe(new Lance(maria, 300.00));
+		leilao.propoe(new Lance(joao, 200.00));
+		leilao.propoe(new Lance(maria, 100.00));
+		
+		Avaliador leiloeiro = new Avaliador();
+		leiloeiro.avalia(leilao);
+		
+		double menorEsperado = 100;
+		double maiorEsperado = 400;
+		
+		assertEquals(maiorEsperado, leiloeiro.getMaiorLance(), 0.00001);
+		assertEquals(menorEsperado, leiloeiro.getMenorLance(), 0.00001);
+	}
+	
+	@Test
+	public void deveEntenderLancesEmOrdemAleatoria() {
+		Usuario joao = new Usuario("João");
+		Usuario maria = new Usuario("Maria");
+		
+		Leilao leilao = new Leilao("RTX 2080 TI");
+		
+		leilao.propoe(new Lance(joao, 200.00));
+		leilao.propoe(new Lance(maria, 450.00));
+		leilao.propoe(new Lance(joao, 120.00));
+		leilao.propoe(new Lance(maria, 700.00));
+		leilao.propoe(new Lance(joao, 630.00));
+		leilao.propoe(new Lance(maria, 230.00));
+		
+		Avaliador leiloeiro = new Avaliador();
+		leiloeiro.avalia(leilao);
+		
+		double menorEsperado = 120;
+		double maiorEsperado = 700;
+		
+		assertEquals(maiorEsperado, leiloeiro.getMaiorLance(), 0.00001);
+		assertEquals(menorEsperado, leiloeiro.getMenorLance(), 0.00001);
+	}
+	
+	@Test
+	public void deveEntenderLeilaoComApenasUmLance() {
+		Usuario joao = new Usuario("João");
+		
+		Leilao leilao = new Leilao("RTX 2080 TI");
+		
+		leilao.propoe(new Lance(joao, 1000.00));
+		
+		Avaliador leiloeiro = new Avaliador();
+		leiloeiro.avalia(leilao);
+
+		assertEquals(1000, leiloeiro.getMaiorLance(), 0.00001);
+		assertEquals(1000, leiloeiro.getMenorLance(), 0.00001);
+	}
+	
+	@Test
+	public void deveEncontrarOsTresMaioresLances() {
+		Usuario joao = new Usuario("João");
+		Usuario maria = new Usuario("Maria");
+		
+		Leilao leilao = new Leilao("RTX 2080 TI");
+		
+		leilao.propoe(new Lance(joao, 100.00));
+		leilao.propoe(new Lance(maria, 200.00));
+		leilao.propoe(new Lance(joao, 300.00));
+		leilao.propoe(new Lance(maria, 400.00));
+		
+		Avaliador leiloeiro = new Avaliador();
+		leiloeiro.avalia(leilao);
+		
+		List<Lance> maiores = leiloeiro.getTresMaiores();
+		
+		assertEquals(3, maiores.size());
+		assertEquals(400.00, maiores.get(0).getValor(), 0.00001);
+		assertEquals(300.00, maiores.get(1).getValor(), 0.00001);
+		assertEquals(200.00, maiores.get(2).getValor(), 0.00001);
 	}
 }
